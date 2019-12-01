d1015a6f4f58091dcc610af22fd430932739fc57
ITmunchies123 <57694016+ITmunchies123@users.noreply.github.com>
2019-12-01 18:52:31 -0500

    delete app

--- munchiesApp/SubSystems/Server/db/FoodSchema.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-
-var foodSchema = mongoose.Schema({
-    'id':{type:String,required:true},
-    'place':{type:String,required:true}
-})
-
-module.exports = mongoose.model('FoodSchema',foodSchema)
--- munchiesApp/SubSystems/Server/server.js
+++
@@ -1,5 +0,0 @@
-const http = require('http');
-const app = require('./app')
-const server = http.createServer(app);
-server.listen(process.env.PORT || 8000);
-console.log('Server running on port 8000.');
